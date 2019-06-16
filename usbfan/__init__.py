a21407b529d98082ace72cf3c73ed3601dd43052
FergoFrog <45412+fergofrog@users.noreply.github.com>
2019-06-16 10:35:02 +1000

    Change how modes are passed into messages
     * Move modes to enums
     * Allow them to be passed as parameters to Messages
     * Update documentation

--- usbfan/__init__.py
+++ usbfan/__init__.py
@@ -1,3 +1,4 @@
 from .message import TextMessage
-from .protocol import Colour, Column, Message, Program
+from .protocol import Colour, Column, Message, Program, MessageStyle, \
+    OpenTransition, CloseTransition
 from .usb import Device
