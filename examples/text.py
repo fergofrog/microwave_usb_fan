2148e70b2eb783412358b6c0a28ca73fe9370a75
trimbonz <trimbonz@users.noreply.github.com>
2022-12-08 17:11:38 +1300

    Updated Text example to demo colour capability

--- examples/text.py
+++ examples/text.py
@@ -1,12 +1,45 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from usbfan import Device, Program, TextMessage
+from usbfan import Colour, ColourTheme, Column, Device, Message, Program, TextMessage, \
+    MessageStyle, OpenTransition, CloseTransition
 
 # A program is made up of a list of Messages
 # A "TextMessage" is a subclass of the generic Message class 
-p = Program((TextMessage("Hello, World!"),
-             TextMessage("How is everyone going?"),))
+
+# "colour" can be one of:
+# Colour.red
+# Colour.yellow
+# Colour.green
+# Colour.cyan
+# Colour.blue
+# Colour.magenta
+# Colour.white
+
+# "theme" can be one of:
+# ColourTheme.Ice
+# ColourTheme.Fire
+# ColourTheme.Festive
+# ColourTheme.Christmas
+# ColourTheme.Rainbow
+
+p = Program((TextMessage( "... HELLO WORLD! ...",
+                theme = ColourTheme.Festive,
+                message_style = MessageStyle.Anticlockwise,
+                open_transition = OpenTransition.ToMiddle,
+                close_transition = CloseTransition.DownUp),
+             TextMessage( "Have a nice day :>",
+                colour = Colour.yellow,
+                message_style = MessageStyle.Flash ),
+             TextMessage( "STAY COOOOOOL :>",
+                theme = ColourTheme.Ice,
+                open_transition = OpenTransition.DownUp,
+                close_transition = CloseTransition.DownUp ),
+             TextMessage( "MERRY CHRISTMAS",
+                theme = ColourTheme.Christmas,
+                message_style = MessageStyle.Clockwise,
+                open_transition = OpenTransition.FromMiddle,
+                close_transition = CloseTransition.ToMiddle) ))
              
 # Open the device and program
 d = Device()
