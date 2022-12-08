38b3508b1da7f5fd26d30dde4c181ca5d6b529a0
trimbonz <trimbonz@users.noreply.github.com>
2022-12-08 16:55:57 +1300

    Improved the rainbow example to give wider colour bands

--- examples/rainbow.py
+++ examples/rainbow.py
@@ -4,13 +4,18 @@
 from usbfan import Colour, Column, Device, Message, Program, TextMessage
 
 # We can cycle the rainbow here and fill all 144 columns
-rainbow_colours = [Colour.red, Colour.yellow, Colour.green,
-                   Colour.cyan, Colour.blue, Colour.magenta]
+rainbow_colours = [Colour.red, Colour.red, Colour.red, Colour.red,
+                   Colour.yellow, Colour.yellow, Colour.yellow, Colour.yellow, 
+                   Colour.green, Colour.green, Colour.green, Colour.green,
+                   Colour.cyan, Colour.cyan, Colour.cyan, Colour.cyan, 
+                   Colour.blue, Colour.blue, Colour.blue, Colour.blue,
+                   Colour.magenta, Colour.magenta, Colour.magenta, Colour.magenta]
 rainbow = [Column([True] * 11,
                   rainbow_colours[i % len(rainbow_colours)])
            for i in range(Message.MAX_COLUMNS)]
 p = Program((
-    TextMessage("Here comes the rainbow!"),
+    TextMessage("Here comes the rainbow!",
+                colour = Colour.blue ),
     Message(rainbow),
 ))
 
